5054dd77e0812b7ddde0e983adcb8bc1e236e915
HockJC <jconliner67@gmail.com>
2024-02-22 06:04:21 +0000

    format the file and beatify the output pics

--- src/collinearity_analysis.py
+++ src/collinearity_analysis.py
@@ -1,10 +1,15 @@
+"""
+This script make collinearity analysis based on 4 predictors for our linear regression
+"""
+
 import pandas as pd
 from scipy.stats import pearsonr
 import matplotlib.pyplot as plt
 import seaborn as sns
 import fire
-def main():
-    dataset_path = './data/static_analysis_data/dataset.csv'
+
+def collinearity_analysis(dataset_path:str,barchart_name:str,heatmap_name:str):
+    
     dataset = pd.read_csv(dataset_path)
 
     funcs_number = dataset['#funcs']
@@ -12,13 +17,12 @@ def main():
     proptery_based_test_funcs_number = dataset['#proptery_based']
     fuzz_target_number = dataset['#fuzz_target']
 
-
     correlation_unit_funcs = pearsonr(funcs_number, unit_test_funcs_number)[0]
     correlation_proptery_based_funcs = pearsonr(funcs_number, proptery_based_test_funcs_number)[0]
     correlation_fuzz_target_funcs = pearsonr(funcs_number, fuzz_target_number)[0]
 
     correlations = [correlation_unit_funcs,correlation_proptery_based_funcs,correlation_fuzz_target_funcs]
-    predictors = ["unit test functions number","property-based test functions number","fuzz target number"]
+    predictors = ['#unit', '#proptery_based', '#fuzz_target']
     
     print(f"Correlation Coefficient of funcs number and unit test functions number : {correlation_unit_funcs:.4f}")
     print(f"Correlation Coefficient of funcs number and property-based test functions number : {correlation_proptery_based_funcs:.4f}")
@@ -26,29 +30,34 @@ def main():
 
     plt.figure(figsize=(10, 6))
     plt.bar(predictors, correlations, color=['blue', 'green', 'red'])
-
-    plt.title('Correlation with Funcs Number')
-    plt.xlabel('Predictors')
-    plt.ylabel('Pearson Correlation Coefficient')
-
+    font = {'family': 'serif',
+        'color':  'darkred',
+        'weight': 'normal',
+        'size': 16,
+        }
+    plt.xlabel('Predictors',fontdict=font)
+    plt.ylabel('Pearson Correlation Coefficient',fontdict=font)
     for i, corr in enumerate(correlations):
-        plt.text(i, corr if corr > 0 else corr - 0.02, f'{corr:.4f}', ha = 'center', va = 'bottom' if corr > 0 else 'top')
-
-    plt.savefig("./Analysis/correlation_coefficient.png",dpi=500)
+        plt.text(i, corr if corr > 0 else corr - 0.014, f'{corr:.4f}', ha = 'center', va = 'bottom')
+    plt.savefig(f"./src/{barchart_name}.png",dpi=500)
     plt.show()
     
     columns_of_interest = ['#funcs', '#unit', '#proptery_based', '#fuzz_target']
-
     correlation_matrix = dataset[columns_of_interest].corr()
 
     sns.set_theme(style="whitegrid")
-
     plt.figure(figsize=(10, 8))
     sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', linewidths=.5)
-    plt.title('Predictors Correlation Heatmap')
-
-    plt.savefig("./Analysis/collinearity_heatmap.png",dpi=500)
-    plt.show()
-
+    plt.savefig(f"./src/{heatmap_name}.png",dpi=500)
+    plt.show()    
+    
+    
+def main():
+    dataset_path = './data/static_analysis_data/dataset.csv'
+    barchart_name = "correlation_coefficient"
+    heatmap_name = "collinearity_heatmap"
+    collinearity_analysis(dataset_path,barchart_name,heatmap_name)
+    
+    
 if __name__ == "__main__":
     fire.Fire(main)
