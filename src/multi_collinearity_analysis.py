0460f98b677751852f44eb01571c6c745f8b7293
HockJC <jconliner67@gmail.com>
2024-03-17 04:47:28 +0000

    format the code

--- src/multi_collinearity_analysis.py
+++ src/multi_collinearity_analysis.py
@@ -5,18 +5,16 @@ This script make collinearity analysiss between certain testing framework and th
 import pandas as pd
 from scipy.stats import pearsonr
 import matplotlib.pyplot as plt
-import seaborn as sns
 import fire
 import os
 import pandas as pd
 import statsmodels.api as sm
 
+
 def build_multilinear_regression_model(
     dataset_path: str = "./data/static_analysis_data/dataset.csv",
-    barchart_name: str = "correlation_coefficient",
-    heatmap_name: str = "collinearity_heatmap",
     output_dir: str = "images",
-    choice: int = 1
+    choice: int = 1,
 ):
 
     dataset = pd.read_csv(dataset_path)
@@ -28,15 +26,37 @@ def build_multilinear_regression_model(
     proptery_based_test_funcs_number = dataset["#proptery_based"]
     fuzz_target_number = dataset["#fuzz_target"]
     if choice == 1:
-        X = pd.concat([files_number,lines_number,funcs_number,unit_test_funcs_number],axis=1)
+        X = pd.concat(
+            [files_number, lines_number, funcs_number, unit_test_funcs_number], axis=1
+        )
     elif choice == 2:
-        X = pd.concat([files_number,lines_number,funcs_number,proptery_based_test_funcs_number],axis=1)
+        X = pd.concat(
+            [
+                files_number,
+                lines_number,
+                funcs_number,
+                proptery_based_test_funcs_number,
+            ],
+            axis=1,
+        )
     elif choice == 3:
-        X = pd.concat([files_number,lines_number,funcs_number,fuzz_target_number],axis=1)
+        X = pd.concat(
+            [files_number, lines_number, funcs_number, fuzz_target_number], axis=1
+        )
     elif choice == 4:
-        X = pd.concat([files_number,lines_number,funcs_number,unit_test_funcs_number,proptery_based_test_funcs_number,fuzz_target_number],axis=1)
-    # X = dataset[["#files", "#lines", "#funcs", "#unit"]] 
-    y = fuzz_target_number  
+        X = pd.concat(
+            [
+                files_number,
+                lines_number,
+                funcs_number,
+                unit_test_funcs_number,
+                proptery_based_test_funcs_number,
+                fuzz_target_number,
+            ],
+            axis=1,
+        )
+    # X = dataset[["#files", "#lines", "#funcs", "#unit"]]
+    y = fuzz_target_number
 
     X = sm.add_constant(X)
 
@@ -44,22 +64,26 @@ def build_multilinear_regression_model(
 
     print(model.summary())
 
-    #residual analysis
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    # residual analysis
     plt.figure(figsize=(10, 5))
     plt.subplot(1, 2, 1)
     plt.scatter(model.fittedvalues, model.resid)
-    plt.xlabel('Fitted values')
-    plt.ylabel('Residuals')
-    plt.title('Residuals vs Fitted')
+    plt.xlabel("Fitted values")
+    plt.ylabel("Residuals")
+    plt.title("Residuals & Fitted Value")
 
     plt.subplot(1, 2, 2)
-    sm.qqplot(model.resid, line='s', ax=plt.gca())
-    plt.title('Normal Q-Q')
+    sm.qqplot(model.resid, line="s", ax=plt.gca())
+    plt.title("Normal Q-Q")
 
     plt.tight_layout()
     plt.savefig(f"{output_dir}/residual_analysis.png")
     plt.show()
 
-#unit testing 1, property testing 2, fuzz testing 3, all testing 4
+
+# unit testing 1, property testing 2, fuzz testing 3, all testing 4
 if __name__ == "__main__":
     fire.Fire(build_multilinear_regression_model)
